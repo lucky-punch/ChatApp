a52e196dc041af835ea2cbd328230b100aeaea2a
suzutaka <ess.discussion@gmail.com>
2018-11-04 13:59:53 +0900

    Initial Commit

---
+++ ChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by nowall on 2018/11/04.
+//  Copyright © 2018 鈴木貴大. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
